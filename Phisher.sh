ddf8507451748c63249ab912e092063300492eb7
Yasser Tahiri <yasserth19@gmail.com>
2020-07-05 12:32:16 +0100

    Update Phisher.sh

--- Phisher.sh
+++ Phisher.sh
@@ -147,7 +147,7 @@ printf "\n"
 printf "\e[1;93m       .:.:.\e[0m\e[1;77m Phishing Tool coded by: Yezz123 \e[0m\e[1;93m.:.:.\e[0m\n"
 printf "\n"
 printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
-printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by ShellPhish ::\e[0m\n"
+printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by Phisher ::\e[0m\n"
 printf "\n"
 }
 
