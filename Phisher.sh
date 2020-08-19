a3b725c45b535169c30b0a1288ec3e4cb148c0b7
Yasser Tahiri <yasserth19@gmail.com>
2020-08-19 04:19:35 +0100

    Edit ⚡

--- Phisher.sh
+++ Phisher.sh
@@ -147,7 +147,7 @@ printf "\n"
 printf "\e[1;93m       .:.:.\e[0m\e[1;77m Phishing Tool coded by: Yezz123 \e[0m\e[1;93m.:.:.\e[0m\n"
 printf "\n"
 printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
-printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by Phisher ::\e[0m\n"
+printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by Phisher take care please !::\e[0m\n"
 printf "\n"
 }
 
@@ -370,7 +370,7 @@ checkfound
 checkfound() {
 
 
-printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting victim open the link ...\e[0m\n"
+printf "\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Waiting victim to open the link ...\e[0m\n"
 while [ true ]; do
 
 
